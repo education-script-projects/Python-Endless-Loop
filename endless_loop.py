98b1497c488b5cd1bc7031b5b2705bb0850a86df
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-11-26 22:17:02 +0300

    master

---
+++ endless_loop.py
@@ -0,0 +1,14 @@
+#!/usr/bin/evy python
+# -*- coding:utf-8 -*-
+
+## Python Code Example
+## Sonsuz
+
+def function():
+    value = 0.1
+    wanted = 0.2
+    while value < wanted:
+        message = "Endless Loop ;)"
+        print(message)
+
+function()
